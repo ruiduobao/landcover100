211cfddc5bc1da598b2c9fb6d8bed15b48a2ca90
kitmyfaceplease <kitmyfaceplease2@gmail.com>
2023-11-06 18:12:32 +0800

    添加地级单位加载

--- app.js
+++ app.js
@@ -20,6 +20,143 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
+//设置一个数据库属性
+const pgp = require('pg-promise')();
+const dbConfig = {
+    host: 'localhost',
+    port: 5434,
+    database: 'postgis_34_sample',
+    user: 'postgres',
+    password: '12345678'
+};
+const db = pgp(dbConfig);
+
+// 新增路由来处理地理编码请求
+app.post('/getGeoAddress', async (req, res, next) => {
+  const placeName = req.body.address; // 从请求体中获取地址
+
+  // 检查place参数是否有效
+  if (!placeName || placeName.trim() === '') {
+      const error = new Error('Invalid place parameter.');
+      return next(error);
+  }
+  const GAODE_API_KEY = 'b6ba147ffd1e49158d12f7cb16d0f381';
+  const GAODE_GEOCODE_URL = `https://restapi.amap.com/v3/geocode/geo?address=${encodeURIComponent(placeName)}&key=${GAODE_API_KEY}`;
+
+  let location = null;
+
+  try {
+      const response = await axios.get(GAODE_GEOCODE_URL);
+      if (response.data && response.data.geocodes && response.data.geocodes.length > 0) {
+          location = response.data.geocodes[0].location;
+      } else {
+          throw new Error('geo code failed');
+      }
+  } catch (error) {
+      return next(error);
+  }
+
+  if (location) {
+      const [longitude, latitude] = location.split(',');
+      // 调用函数来创建并保存 GeoJSON 文件
+      try {
+          const filepath = await createAndSaveGeoJSON(placeName, longitude, latitude);
+          console.log('GeoJSON file created successfully at:', filepath);
+          // 构造文件 URL
+          const fileUrl = `/vectordata/${encodeURIComponent(placeName)}.gson`;  // 相对于服务器根目录的URL路径
+          // 返回成功消息和文件 URL
+          // 返回成功消息和文件 URL
+          const responseData = { status: 'success', message: 'Data exported successfully', filepath: fileUrl };
+          console.log('Response data:', responseData);  // 打印响应数据
+          console.log('hello，world0'); 
+          res.json(responseData);
+      } catch (err) {
+          console.error('Error writing GeoJSON file:', err);
+          res.status(500).json({ status: 'error', message: 'Internal Server Error', filepath: null });
+      }
+  } else {
+      res.status(404).json({ status: 'error', message: `Not found gson for ${placeName}`, filepath: null });
+  }
+});
+
+
+// 从数据库中导出矢量文件到路径
+app.post('/getGsonDB', async (req, res) => {
+  const dataCode = req.body.code;
+
+  if (!dataCode) {
+      return res.status(400).send('dataCode is required');
+  }
+  try {
+      // 6位数字且后4位为0000
+      if (/^\d{2}0000$/.test(dataCode)) {
+          const sql = 'SELECT ST_AsGeoJSON(geom) as geojson_geom, * FROM "SHENG"."CHN_sheng_2023" WHERE first_gid = $1';
+          const results = await db.any(sql, [dataCode]);
+          return sendResults(res, dataCode, results);
+      }
+      // 6位数字且后2位为00
+      else if (/^\d{4}00$/.test(dataCode)) {
+          const results = await queryLatestYearData('SHI', 'CHN_shi', dataCode);
+          return sendResults(res, dataCode, results);
+      }
+      // 6位数字且后两位不为00
+      else if (/^\d{6}$/.test(dataCode) && !/00$/.test(dataCode)) {
+          const results = await queryLatestYearData('XIAN', 'CHN_xian', dataCode);
+          return sendResults(res, dataCode, results);
+      }
+      // 12位数字
+      else if (/^\d{12}$/.test(dataCode)) {
+          const sql = 'SELECT ST_AsGeoJSON(geom) as geojson_geom, * FROM "XIANG"."CHN_xiang_2020" WHERE code = $1';
+          const results = await db.any(sql, [dataCode]);
+          return sendResults(res, dataCode, results);
+      }
+      else {
+          return res.status(400).send('Invalid dataCode format');
+      }
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+//发送矢量文件导出的路径结果
+function sendResults(res, dataCode, results) {
+  if (results.length > 0) {
+      const geojsonFeatureCollection = {
+          type: "FeatureCollection",
+          features: results.map(result => {
+              const geojsonGeom = JSON.parse(result.geojson_geom);
+              delete result.geojson_geom;
+              delete result.geom;
+              return {
+                  type: "Feature",
+                  geometry: geojsonGeom,
+                  properties: result
+              };
+          })
+      };
+      const gsonFilePath = path.join(__dirname, 'public', 'vector_fromDB', `${dataCode}.gson`);
+      fs.writeFileSync(gsonFilePath, JSON.stringify(geojsonFeatureCollection));
+      const gsonFileUrl = `/vector_fromDB/${dataCode}.gson`;
+      res.json({ status: 'success', message: 'Data exported successfully', filepath: gsonFileUrl });
+  } else {
+      res.status(404).send('Data not found');
+  }
+}
+
+//获取最近年份的行政代码
+async function queryLatestYearData(schema, baseTableName, dataCode) {
+  const years = Array.from({ length: 11 }, (_, i) => 2023 - i);
+  for (let year of years) {
+      const tableName = `${baseTableName}_${year}`;
+      const sql = `SELECT ST_AsGeoJSON(geom) as geojson_geom, * FROM "${schema}"."${tableName}" WHERE code = $1`;
+      const results = await db.any(sql, [dataCode]);
+      if (results.length > 0) {
+          return results;
+      }
+  }
+  return [];
+}
 //POST路由，处理从前端接收到的GeoJSON数据并保存到文件
 app.post('/savegeojson', (req, res) => {
   // 假设我们从请求体中得到了GeoJSON数据
--- public/js/select_shengshixian.js
+++ public/js/select_shengshixian.js
@@ -1,19 +1,42 @@
-// //设置用户上传功能
-// function handleSelectChange() {
-//     const selectElement = document.getElementById('data_type_select');
-//     const fileUploadContainer = document.getElementById('file-upload-container');
-//     const drawIcons = document.getElementById('drawIcons');
-//     // 隐藏所有相关的div
-//     fileUploadContainer.style.display = 'none';
-//     drawIcons.style.display = 'none';
-    
-//     // 根据选中的选项显示相应的div
-//     if (selectElement.value === 'type2') { // 如果是本地上传
-//         fileUploadContainer.style.display = 'block';
-//     } else if (selectElement.value === 'type3') { // 如果是在线勾画
-//         drawIcons.style.display = 'flex';
-//     }
-// }
+//省份和对应代码
+const provinceCodes = {
+    '北京市': '110000',
+    '天津市': '120000',
+    '河北省': '130000',
+    '山西省': '140000',
+    '内蒙古自治区': '150000',
+    '辽宁省': '210000',
+    '吉林省': '220000',
+    '黑龙江省': '230000',
+    '上海市': '310000',
+    '江苏省': '320000',
+    '浙江省': '330000',
+    '安徽省': '340000',
+    '福建省': '350000',
+    '江西省': '360000',
+    '山东省': '370000',
+    '河南省': '410000',
+    '湖北省': '420000',
+    '湖南省': '430000',
+    '广东省': '440000',
+    '广西壮族自治区': '450000',
+    '海南省': '460000',
+    '重庆市': '500000',
+    '四川省': '510000',
+    '贵州省': '520000',
+    '云南省': '530000',
+    '西藏自治区': '540000',
+    '陕西省': '610000',
+    '甘肃省': '620000',
+    '青海省': '630000',
+    '宁夏回族自治区': '640000',
+    '新疆维吾尔自治区': '650000',
+    '香港特别行政区': '810000',
+    '澳门特别行政区': '820000',
+    '台湾省': '710000'
+    };
+
+
 //按选中区域选择功能进行显示
 function handleSelectChange() {
     var selectedValue = document.getElementById('data_type_select').value;
@@ -22,19 +45,24 @@ function handleSelectChange() {
     var drawIconsContainer = document.getElementById('drawIcons');
 
     // 隐藏所有其他选项
-    administrativeContainer.style.display = 'none';
+    administrativeContainer.style.display = 'block';
     fileUploadContainer.style.display = 'none';
     drawIconsContainer.style.display = 'none';
 
     switch(selectedValue) {
         case 'type1': // 当选中按行政区划时
-            administrativeContainer.style.display = 'block';
+            fileUploadContainer.style.display = 'none';
+            drawIconsContainer.style.display = 'none';
             break;
         case 'type2': // 当选中本地上传时
             fileUploadContainer.style.display = 'block';
+            administrativeContainer.style.display = 'none';
+            drawIconsContainer.style.display = 'none';
             break;
         case 'type3': // 当选中在线勾画时
             drawIconsContainer.style.display = 'block';
+            fileUploadContainer.style.display = 'none';
+            administrativeContainer.style.display = 'none';
             break;
     }
 }
@@ -42,4 +70,130 @@ function handleSelectChange() {
 // 页面加载完成后绑定change事件到select框
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('data_type_select').addEventListener('change', handleSelectChange);
+    fillProvinceSelect();
+    // 设置默认选项为type1，并触发change事件以显示省级选择框
+    document.getElementById('data_type_select').value = 'type1';
+    handleSelectChange();
+    // 一开始就为city_select元素添加事件监听器
+    const citySelect = document.getElementById('city_select');
+    citySelect.removeEventListener('change', citySelectChangeHandler); // 移除旧的
+    citySelect.addEventListener('change', citySelectChangeHandler); // 添加新的
 });
+
+// 填充省级select的函数
+function fillProvinceSelect() {
+    console.log('选择省份'); 
+    var provinceSelect = document.getElementById('province_select');
+    for (var province in provinceCodes) {
+        var option = document.createElement('option');
+        option.value = provinceCodes[province];
+        option.text = province;
+        provinceSelect.appendChild(option);
+    }
+
+    // 添加change事件监听器到province_select，并将查询到的矢量加载到地图中
+    provinceSelect.addEventListener('change', function() {
+        var sheng_code = this.value;
+        console.log('选中的省级代码是:', sheng_code);
+
+        // 在这里读取和解析shengshi.md文件，然后更新城市选择框
+        fetch('/shengshixian/shengshi.md')
+            .then(response => response.text())
+            .then(text => {
+                updateCitySelect(text, sheng_code);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+        console.log('选中的省级代码是:', sheng_code); // 或者你可以在这里做其他事情
+
+        // 调用POST路由来获取矢量数据
+        fetch('/getGsonDB', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code: sheng_code }) // 发送省份代码
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                // 使用从POST路由获得的路径加载矢量
+                loadGeoJSONfromPath(data.filepath);
+            } else {
+                throw new Error(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+}
+
+// 用于更新城市选择框的函数
+function updateCitySelect(mdContent, provinceCode) {
+    const citySelect = document.getElementById('city_select');
+    // 移除旧的事件监听器以防止重复添加
+    citySelect.removeEventListener('change', citySelectChangeHandler);
+    citySelect.innerHTML = ''; // 清空现有的城市列表
+    
+    // 解析Markdown内容
+    const lines = mdContent.split('\n');
+    let provinceFound = false;
+    for (const line of lines) {
+        // 检查是否为所需的省份
+        if (line.startsWith('# ') && line.includes(provinceCode)) {
+            provinceFound = true;
+            continue; // 省份已找到，跳过添加，接下来添加城市
+        }
+        
+        // 如果省份已找到，开始查找城市
+        if (provinceFound && line.startsWith('## ')) {
+            const cityInfo = line.match(/## (.*) (\d+)/);
+            if (cityInfo) {
+                const cityName = cityInfo[1];
+                const cityCode = cityInfo[2];
+                const option = document.createElement('option');
+                option.value = cityCode;
+                option.text = cityName;
+                citySelect.appendChild(option);
+            }
+        }
+
+        // 如果下一个省份开始，则停止添加城市
+        if (provinceFound && line.startsWith('# ') && !line.includes(provinceCode)) {
+            break;
+        }
+    }
+    //添加选中的市级单位到地图上
+    // 添加新的事件监听器
+    citySelect.addEventListener('change', citySelectChangeHandler);
+}
+
+// 市级选择框变化时的处理函数
+function citySelectChangeHandler() {
+    var city_code = this.value;
+    console.log('选中的市级代码是:', city_code); // 打印到控制台
+    // 如果你需要将选中的城市代码显示在页面上，你可以在这里实现
+    // 调用POST路由来获取矢量数据
+    fetch('/getGsonDB', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ code: city_code}) // 发送省份代码
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            // 使用从POST路由获得的路径加载矢量
+            loadGeoJSONfromPath(data.filepath);
+        } else {
+            throw new Error(data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
